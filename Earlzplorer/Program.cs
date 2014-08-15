8db1a7d01270f0a2ce4135e4ef2eae20fd09dd3b
Earlz <earlz@lastyearswishes.com>
2014-08-15 01:01:16 -0400

    create

---
+++ Earlzplorer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Earlzplorer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
